6f71a667716c914700a358c0f291c58767495073
nahsiloh <lishan1512@gmail.com>
2020-02-05 22:27:55 +0800

    added tests

---
+++ src/components/CreateUser/CreateUser.test.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import CreateUser from "./CreateUser";
+import { BrowserRouter as Router } from "react-router-dom";
+
+jest.mock("axios");
+
+describe("Create Account", () => {
+  describe("display elements", () => {
+    it("should have Login title on page ", () => {
+      const { getByText } = render(
+        <Router>
+          <CreateUser />
+        </Router>
+      );
+      expect(getByText("Create Account")).toBeInTheDocument();
+    });
+
+    it("should be able to see the email, username and password input fields", () => {
+      const { getByPlaceholderText } = render(
+        <Router>
+          <CreateUser />
+        </Router>
+      );
+      expect(getByPlaceholderText("Email")).toBeInTheDocument();
+      expect(getByPlaceholderText("Username")).toBeInTheDocument();
+      expect(getByPlaceholderText("Password")).toBeInTheDocument();
+    });
+  });
+
+  it("should display a create button", () => {
+    const { getByTestId } = render(
+      <Router>
+        <CreateUser />
+      </Router>
+    );
+    expect(getByTestId("create__button")).toBeInTheDocument();
+  });
+
+  describe("enter inputs", () => {
+    it("should fill in the create account inputs", () => {
+      const handleCreateUserChange = jest.fn();
+      const { getByPlaceholderText } = render(
+        <Router>
+          <CreateUser handleLoginChange={handleCreateUserChange} />
+        </Router>
+      );
+      const email = getByPlaceholderText("Email");
+      fireEvent.change(email, {
+        target: { value: "testUser@mail.com" }
+      });
+
+      const username = getByPlaceholderText("Username");
+      fireEvent.change(username, {
+        target: { value: "testUser" }
+      });
+
+      const userPassword = getByPlaceholderText("Password");
+      fireEvent.change(userPassword, { target: { value: "pass1234" } });
+
+      expect(email.value).toBe("testUser@mail.com");
+      expect(username.value).toBe("testUser");
+      expect(userPassword.value).toBe("pass1234");
+    });
+  });
+
+  describe("error message", () => {
+    it("should display error message if failed to create", async () => {
+      const submitCreateUser = jest.fn();
+      const { getByTestId, findByText } = render(
+        <Router>
+          <CreateUser submitCreateUser={submitCreateUser} />
+        </Router>
+      );
+
+      const createButton = getByTestId("create__button");
+      fireEvent.click(createButton);
+
+      const createErrorMessage = await findByText(
+        "Unable to create new account :("
+      );
+      expect(createErrorMessage).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/components/LoginForm/LoginForm.test.js
@@ -0,0 +1,88 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent } from "@testing-library/react";
+import LoginForm from "./LoginForm";
+import { BrowserRouter as Router } from "react-router-dom";
+
+jest.mock("axios");
+
+describe("Login Form", () => {
+  describe("display elements", () => {
+    it("should have Login title on page ", () => {
+      const { getByText } = render(
+        <Router>
+          <LoginForm />
+        </Router>
+      );
+      expect(getByText("User Login")).toBeInTheDocument();
+    });
+
+    it("should display the email and password input", () => {
+      const { getByPlaceholderText } = render(
+        <Router>
+          <LoginForm />
+        </Router>
+      );
+      expect(getByPlaceholderText("Username")).toBeInTheDocument();
+      expect(getByPlaceholderText("Password")).toBeInTheDocument();
+    });
+
+    it("should display a login button", () => {
+      const { getByTestId } = render(
+        <Router>
+          <LoginForm />
+        </Router>
+      );
+      expect(getByTestId("login__button")).toBeInTheDocument();
+    });
+
+    it("should display a create new account button", () => {
+      const { getByTestId } = render(
+        <Router>
+          <LoginForm />
+        </Router>
+      );
+      expect(getByTestId("createAccount__button")).toBeInTheDocument();
+    });
+  });
+
+  describe("enter inputs", () => {
+    it("should fill in the login credentials", () => {
+      const handleLoginChange = jest.fn();
+      const { getByPlaceholderText } = render(
+        <Router>
+          <LoginForm handleLoginChange={handleLoginChange} />
+        </Router>
+      );
+      const username = getByPlaceholderText("Username");
+      fireEvent.change(username, {
+        target: { value: "testUser" }
+      });
+
+      const userPassword = getByPlaceholderText("Password");
+      fireEvent.change(userPassword, { target: { value: "pass1234" } });
+
+      expect(username.value).toBe("testUser");
+      expect(userPassword.value).toBe("pass1234");
+    });
+  });
+
+  describe("confirmation message", () => {
+    it("should display an error message when user failed to logged in", async () => {
+      const loginSubmit = jest.fn();
+      const { getByTestId, findByText } = render(
+        <Router>
+          <LoginForm loginSubmit={loginSubmit} />
+        </Router>
+      );
+
+      const button = getByTestId("login__button");
+      fireEvent.click(button);
+
+      const loginErrorMessage = await findByText(
+        "Invalid username or password"
+      );
+      expect(loginErrorMessage).toBeInTheDocument();
+    });
+  });
+});
